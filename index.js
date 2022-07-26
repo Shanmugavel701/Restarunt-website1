5f376264b8c49ef72d0aaff34a0ee8314f965d23
Shanmugavel <81358506+Shanmugavel701@users.noreply.github.com>
2022-07-26 10:20:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var i = 0;
 var images = [];
 var time = 2000;
 
-images[0]= "./images/chikken.png" ;
-images[1]= "./images/noodles.png" ;
-images[2]= "./images/pizza.png" ;
-images[3]= "./images/pngwing.com (3).png" ;
-images[4]= "./images/pngwing.com (1).png" ;
+images[0]= "images/chikken.png" ;
+images[1]= "images/noodles.png" ;
+images[2]= "images/pizza.png" ;
+images[3]= "images/pngwing.com (3).png" ;
+images[4]= "images/pngwing.com (1).png" ;
 function slideshow(){
 document.slide.src = images[i]
    if( i<images.length-1){
